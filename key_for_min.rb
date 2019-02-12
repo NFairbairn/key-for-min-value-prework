f4886b4fd19936b9d0414db3fc57f5e353b16189
Noah Fairbairn <noahfairbairn@gmail.com>
2019-02-12 17:26:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,12 +17,12 @@ def key_for_min_value(name_hash)
   d_value= 1000
   d_key= 0
 
+  if name_hash.empty?
+     return nil
   # Iterate new array
   name_a.each do |i|
       # If current value is lower than default, change value&key
-      if name_hash.empty?
-         return nil
-      elsif i[1] < d_value
+      if i[1] < d_value
         d_value = i[1]
         d_key = i[0]
       end
